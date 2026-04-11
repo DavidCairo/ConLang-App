5300c82a1fcb3ade2212221bd342d7eea4f31ad6
David <david.cairo@icloud.com>
2026-04-11 12:54:34 +0200

    created seperate dictionairy.js for global words

--- lesson1.js
+++ lesson1.js
@@ -1,30 +1,18 @@
-const l1_nouns= {
-    woman: { root: "sidhi", class: "animate" },
-    water: { root: "aaloo", class: "inanimate" },
-    fire: { root: "aahvuu", class: "animate" }
-};
-
-const l1_verbs={
-    love: { stem: "aim", trans: true },
-    laugh: { stem: "aahan", trans: false },
-    sleep: { stem: "iilha", trans: false }
-}
-
 const lesson1Data = [
     {
         lessonId: 1,
         type: "en_to_tv",
         english: "The woman loves water",
-        tvaali: l1_nouns.woman.root + " " + 
-                nounCaser.getAccusative(l1_nouns.water, l1_nouns.water.animate) + " " + 
-                "th" + l1_verbs.love.stem // For now a simple th- as verb.js is not build yet. 
+        tvaali: nouns.woman.root + " " + 
+                nounCaser.getAccusative(nouns.water, "singular") + " " + 
+                "th" + verbs.love.stem // For now a simple th- as verb.js is not build yet. 
     },
     {
         lessonId: 1,
         type: "tv_to_en",
-        tvaali: l1_nouns.woman.root + " " + 
-                nounCaser.getAccusative(l1_nouns.water, l1_nouns.water.animate) + " " + 
-                "th" + l1_verbs.love.stem,
+        tvaali: nouns.woman.root + " " + 
+                nounCaser.getAccusative(nouns.water, "singular") + " " + 
+                "th" + verbs.love.stem,
         english: "the woman loves water"
     },
     {
@@ -32,12 +20,12 @@ const lesson1Data = [
         type: "en_to_tv",
         english: "The woman sleeps/The woman is sleeping",
         // Intransitive: Subject + Verb
-        tvaali: l1_nouns.woman.root + " " + "th" + l1_verbs.sleep.stem
+        tvaali: nouns.woman.root + " " + "th" + verbs.sleep.stem
     },
     {
         lessonId: 1,
         type: "tv_to_en",
-        tvaali: l1_nouns.woman.root + " " + l1_verbs.sleep.stem,
+        tvaali: nouns.woman.root + " " + verbs.sleep.stem,
         english: ["the woman sleeps", "the woman is sleeping"]
     }
 ];
